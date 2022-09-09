ac82b2c1ddd9b5d7b1e196c9ba2cb2e45c973759
sithabisenim <sithabisenimtshali@gmail.com>
2022-09-09 06:15:46 -0700

    Task 7

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -4,7 +4,7 @@
  * main - print the string in the put funtion
  *
  * Description: using the main fuction
- * this program prints "the size of various types on the computer"
+ * this program prints "Programming is like building a multilingual puzzle"
  * Return: 0
  */
 int main(void)
@@ -15,10 +15,10 @@ int main(void)
 	long long lli;
 	float f;
 
-	printf ("Size of a char: %ld bytes(s)\n", sizeof(c));
-	printf ("Size of an int: %ld bytes(s)\n", sizeof(i));
-	printf ("Size of a long int: %ld byte(s)\n", sizeof(li));
-	printf ("Size of a long long int: %ld bytes(s)\n", sizeof(lli));
-	printf ("Size of a float: %ld byte(s)\n", sizeof(f));
+	printf("Size of a char: %ld bytes(s)\n", sizeof(c));
+	printf("Size of an int: %ld bytes(s)\n", sizeof(i));
+	printf("Size of a long int: %ld byte(s)\n", sizeof(li));
+	printf("Size of a long long int: %ld bytes(s)\n", sizeof(lli));
+	printf("Size of a float: %ld byte(s)\n", sizeof(f));
 	return (0);
 }
