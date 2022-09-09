34fa11843febbc9fefe8bb1bbff7af22947f9eae
sithabisenim <sithabisenimtshali@gmail.com>
2022-09-09 06:24:31 -0700

    Task 7

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+
 /**
  * main - print the string in the put funtion
  *
@@ -13,10 +14,10 @@ int main(void)
 	long li;
 	long long lli;
 	float f;
-        printf("Size of a char: %ld bytes(s)\n", sizeof(c));
-	printf("Size of an int: %ld bytes(s)\n", sizeof(i));
-	printf("Size of a long int: %ld byte(s)\n", sizeof(li));
-	printf("Size of a long long int: %ld bytes(s)\n", sizeof(lli));
-	printf("Size of a float: %ld byte(s)\n", sizeof(f));
+        printf("Size of a char: %ld bytes(s)\n",sizeof(c));
+	printf("Size of an int: %ld bytes(s)\n",sizeof(i));
+	printf("Size of a long int: %ld byte(s)\n",sizeof(li));
+	printf("Size of a long long int: %ld bytes(s)\n",sizeof(lli));
+	printf("Size of a float: %ld byte(s)\n",sizeof(f));
 	return (0);
 }
