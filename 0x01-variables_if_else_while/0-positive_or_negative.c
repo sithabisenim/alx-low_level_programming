ba272ebfb797f951f6364d50cff2349364790981
sithabisenim <sithabisenimtshali@gmail.com>
2022-09-12 12:34:13 -0700

    Task 0

---
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -0,0 +1,26 @@
+/**
+ * main - Printd when number is positive, zero, negative
+ *
+ * Return: Always (success)
+ */
+int main(void)
+{
+	int n;
+
+	srand(time(0));
+	n=rand()-RAND_MAX/2;
+
+	if (n>0)
+	{ 
+	  printf("%d is possible\n", n);
+	}
+	else if (n==0)
+	{
+	  printf ("%d is zero\n", n);
+	}
+	else
+	{
+          printf("%d is negative\n", n);
+	}
+	return (0);
+}
