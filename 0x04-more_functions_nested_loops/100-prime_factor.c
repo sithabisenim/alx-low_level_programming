0ddd5e438762804a75328d6dd50a2f43a3a553ee
sithabisenim <sithabisenimtshali@gmail.com>
2022-09-16 21:24:10 -0700

    fgghb

--- 0x04-more_functions_nested_loops/100-prime_factor.c
+++ 0x04-more_functions_nested_loops/100-prime_factor.c
@@ -1,7 +1,8 @@
 #include <stdio.h>
 #include <math.h>
 /**
- * main - finds and prints the largest prime factor of the number 612852475143
+ * main - finds and prints the largest 
+ * prime factor of the number 612852475143
  * followed by a new line
  * Return: Always 0 (Success)
  */
