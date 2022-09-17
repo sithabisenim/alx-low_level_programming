a25fec1d0a07b7ed7d33ac68b86ffd86bce1aea2
sithabisenim <sithabisenimtshali@gmail.com>
2022-09-16 18:09:46 -0700

    Task 9

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -12,6 +12,8 @@ int main(void)
 
 	for (i = 1; i <= 100; i++)
 	{
+		if (i % 3 == 0 && i % 5 != 0)
+		{
 		printf("Fuzz");
 	}
 	else if (i % 5 == 0 && i % 3 != 0)
@@ -29,6 +31,7 @@ int main(void)
 	else
 	{
 		printf("%d", i);
+	}
 	}
 	printf("\n");
 	return (0);
