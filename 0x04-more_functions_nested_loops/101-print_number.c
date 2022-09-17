3b94f6b04fab23e519a25090ca1cdd7b4c174756
sithabisenim <sithabisenimtshali@gmail.com>
2022-09-16 17:57:24 -0700

    Task 12

--- 0x04-more_functions_nested_loops/101-print_number.c
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -1,19 +1,35 @@
 #include "main.h"
+#include <stdio.h>
 /**
-* print_number - prints numbers
-* @n: number to be printed
-* Return:void
-*/
-void print_number(int n)
+ * main - prints the members from 1 to 100, followed by a new line
+ * but forr multiples of three prints Fizz instead of the number
+ * and for the multiples of five prints Buzz
+ * Return: Always 0 (Success)
+ */
+int main(void)
 {
-	unsigned int x;
-	x = n;
-	if (n < 0)
+	int i;
+
+	for (i = 1; i <= 100; i++)
 	{
-		_putchar(45);
-		x = -n;
+		printf("Fuzz");
 	}
-	if (x / 10)
-		print_number(x / 10);
-	_putchar((x % 10) + '0');
+	else if (i % 5 == 0 && i % 3 != 0)
+	{
+		printf("Buzz");
+	}
+	else if (i % 3 == 0 && i % 5 == 0)
+	{
+		printf(" FizzBuzz");
+	}
+	else if (i == 1)
+	{
+		printf("%d", i);
+	}
+	else
+	{
+		printf("%d", i);
+	}
+	printf("\n");
+	return (0);
 }
