3e0bc36d82a2833609c25b46ecbb6ef828914c26
sithabisenim <sithabisenimtshali@gmail.com>
2022-09-19 16:22:31 -0700

    Task 7

---
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,20 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * puts_half - Print half a str
+ * @str: a printer
+ */
+void puts_half(char *str)
+{
+	int index = 0, len = 0, n;
+
+	while (str[index++])
+		len++;
+	if ((len % 2) == 0)
+		n = len / 2;
+	else
+		n = (len + 1) / 2;
+	for (index = n; index < len; index++)
+		putchar(str[index]);
+	putchar('\n');
+}
