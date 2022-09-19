3a36cd9e05d672f00715b783de58c2aeeb94ccda
sithabisenim <sithabisenimtshali@gmail.com>
2022-09-19 16:40:53 -0700

    Task 8

--- 0x05-pointers_arrays_strings/8-print_array.c
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -11,10 +11,10 @@ void print_array(int *a, int n)
 
 	for (index = 0; index < n; index++)
 	{
-		printf("%d", a[index]);
-		if (index == n - 1)
-			continue;
-		printf(", ");
-		printf("\n");
+		if (index != n - 1)
+			printf("%d, ", a[index]);
+		else
+			printf("%d", a[index]);
 	}
+	putchar(10);
 }
