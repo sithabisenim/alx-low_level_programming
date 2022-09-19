6d7b573a8e2fac7b19cc00321b4357c9b2216484
sithabisenim <sithabisenimtshali@gmail.com>
2022-09-19 15:37:10 -0700

    Task 4

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,16 @@
+#include "main.h"
+#include <string.h>
+/**
+ * print_rev - string in reverse
+ * @s: string
+ */
+void print_rev(char *s)
+{
+	int i, len;
+	
+	len = strlen(s);
+	
+	for (i = len - 1; i >= 0; i--)
+		putchar(s[i]);
+	putchar('\n');
+}
