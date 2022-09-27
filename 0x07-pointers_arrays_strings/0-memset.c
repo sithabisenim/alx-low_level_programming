84fadc34422902b7242e65898d93e388b75460cf
sithabisenim <sithabisenimtshali@gmail.com>
2022-09-27 04:56:52 -0700

    Task 0

---
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -0,0 +1,22 @@
+#include "main.h"
+#include <stdio.h>
+
+
+/**
+ *main -function that fills a memory with constant byte
+ *@s: starting adrress
+ *@d: value
+ *@n: no. of bytes
+ *Return: Always 0
+ */
+ char *_memset(char *s, char b, unsigned int n);
+{
+	int i = 0;
+
+	for (; n > 0; i++)
+	{
+		s[i] = b;
+		n--;
+	}
+	return s;
+}
