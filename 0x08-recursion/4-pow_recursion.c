c504d6ca018f540370f586037415b7df4028d82f
sithabisenim <sithabisenimtshali@gmail.com>
2022-09-28 16:39:56 -0700

    Task 4

--- 0x08-recursion/4-pow_recursion.c
+++ 0x08-recursion/4-pow_recursion.c
@@ -10,6 +10,9 @@ int _pow_recursion(int x, int y)
 {
 	if (y < 0)
 		return (-1);
-	else
-		return (_pow_recursion(x, y));
+	if (y == 1)
+		return (x);
+	if (y == 0)
+		return (1);
+	return (x * _pow_recursion(x, y - 1));
 }
