7d5949da07264f92aa811bb2b073ff19454998e0
sithabisenim <sithabisenimtshali@gmail.com>
2022-09-28 16:14:07 -0700

    Task 4

---
+++ 0x08-recursion/4-pow_recursion.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+ * _pow_recursion - function returns value of x raised by y
+ * @x: base
+ * @y: exponent
+ * Return: value of x
+ * if y is less than 0 returns -1
+ */
+int _pow_recursion(int x, int y)
+{
+	if (y < 0)
+		return (-1);
+	if (y > 0)
+		return (x, y);
+}
