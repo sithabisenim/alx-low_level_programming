3d135a6e83f91abd91c024637bcf9bef15015d70
sithabisenim <sithabisenimtshali@gmail.com>
2022-09-28 14:35:28 -0700

    Task 0

---
+++ 0x08-recursion/README.md
@@ -0,0 +1,1 @@
+Recursion going deeper
---
+++ 0x08-recursion/_putchar.c
@@ -0,0 +1,12 @@
+#include "main.h"
+
+/**
+ * _putchar.c - write character c in stdout
+ * @c: character to be printed
+ * Return: on success 1
+ * on error return -1
+ */
+int _putchar(char c)
+{
+	return (write(1, &c, 1);
+}
