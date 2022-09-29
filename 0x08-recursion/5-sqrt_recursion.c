bb02105ea0ccc38622a24cd85df29cce6bfc4d27
sithabisenim <sithabisenimtshali@gmail.com>
2022-09-28 17:52:48 -0700

    Task 5

--- 0x08-recursion/5-sqrt_recursion.c
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -1,4 +1,6 @@
 #include "main.h"
+
+int real_root(int n, int i);
 /**
  * _sqrt_recursion - function return natural square roots
  * @n: number given
@@ -6,10 +8,22 @@
  */
 int _sqrt_recursion(int n)
 {
-	if (n <= 0)
+	if (n < 0)
 		return (-1);
-	int root = _sqrt_recursion(n);
+	return (real_root(n, 0));
+}
 
-	if (root * root == n)
-		return (root);
+/**
+ * real_root - find natural square root of a number
+ * @n: number given
+ * @i: a count
+ * Return: natural root
+ */
+int real_root(int n, int i)
+{
+	if (i * i > n)
+		return (-1);
+	if (i * i == n)
+		return (i);
+	return (real_root(n, i + 1));
 }
