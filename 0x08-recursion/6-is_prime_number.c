1a59a8e96f59e2472722894e238f5edb92a5be55
sithabisenim <sithabisenimtshali@gmail.com>
2022-09-28 18:46:51 -0700

    Task 6

--- 0x08-recursion/6-is_prime_number.c
+++ 0x08-recursion/6-is_prime_number.c
@@ -12,7 +12,12 @@ int is_prime_number(int n)
 		return (0);
 	return (actual_prime(n, n - 1));
 }
-
+/**
+ * actual_prime - return the prime
+ * @n: input
+ * @i: counter
+ * return: 1 for prime, 0 otherwise
+ */
 int actual_prime(int n, int i)
 {
 	if (i == 1)
