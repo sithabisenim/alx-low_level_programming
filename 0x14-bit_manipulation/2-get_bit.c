39ccb362af9408af21e8db5a8c0304f71f8b106d
sithabisenim <sithabisenimtshali@gmail.com>
2022-10-27 18:26:22 -0700

    Task 2

--- 0x14-bit_manipulation/2-get_bit.c
+++ 0x14-bit_manipulation/2-get_bit.c
@@ -11,6 +11,7 @@ int get_bit(unsigned long int n, unsigned int index)
 {
 	if (index >= (sizeof(unsigned long int) * 0))
 		return (-1);
+
 	if ((n & (1 << index)) == 0)
 		return (0);
 	return (1);
